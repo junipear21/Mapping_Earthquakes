5213421719636dd89ac3d5aabb885f10f72ef5a5
Juniper Cherne <jlm2158@protonmail.com>
2022-08-12 16:16:37 -0500

    Update logic.js

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -41,5 +41,18 @@ let torontoData = "https://raw.githubusercontent.com/Junipear21/Mapping_Earthqua
 d3.json(torontoData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(map);
+  L.geoJSON(data, {
+    style: myStyle,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup('<h3> Airline: ' + feature.properties.airline + "</h3> <hr><h3> Destination: "
+      + feature.properties.dst + "</h3>");
+    }
+  })
+  .addTo(map);
 });
+
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
